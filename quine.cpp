da5ee5fbf8a47eb14874935a8701f2929a41cd18
YudNK <38344935+YudNK@users.noreply.github.com>
2023-03-26 22:30:33 +0900

    iostreamに変更

--- quine.cpp
+++ quine.cpp
@@ -1,8 +1,8 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main(){
-    string str="\";\n    cout<<\"#include<bits/stdc++.h>\\nusing namespace std;\\nint main(){\\n    string str=\\\"\";\n    for(char c:str){\n        if(c=='\\n'){cout<<'\\\\'<<'n';}\n        else if(c=='\\\\'){cout<<'\\\\'<<'\\\\';}\n        else if(c=='\\\"'){cout<<'\\\\'<<'\\\"';}\n        else{cout<<c;}\n    }\n    cout<<str;\n}\n";
-    cout<<"#include<bits/stdc++.h>\nusing namespace std;\nint main(){\n    string str=\"";
+    string str="\";\n    cout<<\"#include<iostream>\\nusing namespace std;\\nint main(){\\n    string str=\\\"\";\n    for(char c:str){\n        if(c=='\\n'){cout<<'\\\\'<<'n';}\n        else if(c=='\\\\'){cout<<'\\\\'<<'\\\\';}\n        else if(c=='\\\"'){cout<<'\\\\'<<'\\\"';}\n        else{cout<<c;}\n    }\n    cout<<str;\n}\n";
+    cout<<"#include<iostream>\nusing namespace std;\nint main(){\n    string str=\"";
     for(char c:str){
         if(c=='\n'){cout<<'\\'<<'n';}
         else if(c=='\\'){cout<<'\\'<<'\\';}
